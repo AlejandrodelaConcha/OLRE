0e269d2f495688c78af1dfe7e131571c1d33020f
AlejandrodelaConcha <alejandro.de_la_concha_duarte@ens-paris-saclay.fr>
2024-02-28 13:54:38 +0100

    Create __init__.py

---
+++ Evaluation/__init__.py
@@ -0,0 +1,4 @@
+from .estimate_errors import error_graph,error_graph_offline,estimate_errors_online,estimate_errors_offline
+from .open_results import open_results_online,open_results_offline
+
+__all__ = ["error_graph","error_graph_offline","open_results_online","open_results_offline","estimate_errors_online","estimate_errors_offline"]
